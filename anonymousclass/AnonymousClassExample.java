49da1ed4eb36d17cece91a1691ea8403bcd43987
samnang-loma <samnang.chanserey@lomatechnology.com>
2024-07-17 10:00:46 +0700

    anonymous class example

---
+++ anonymousclass/AnonymousClassExample.java
@@ -0,0 +1,49 @@
+package anonymousclass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + age + ")";
+    }
+}
+
+public class AnonymousClassExample {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Alice", 30));
+        people.add(new Person("Bob", 25));
+        people.add(new Person("Charlie", 35));
+
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return Integer.compare(p1.getAge(), p2.getAge());
+            }
+        });
+
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
+}
